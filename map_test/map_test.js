134da0f24d492f5474eeaeeac99b0772806e0cf3
nymphofthevales <81981344+nymphofthevales@users.noreply.github.com>
2021-07-20 17:56:04 -0600

    begin mapping logic

--- map_test/map_test.js
+++ map_test/map_test.js
@@ -4,7 +4,7 @@ function Grid(w,h,s) {
     this.cellSize = s;
     this.array = [];
     for (let i=1; i <= (w * h); i++) {
-        this.array.push(new MapTile(i,this))
+        this.array.push(new MapTile(i,this,0))
     }
 }
 Grid.prototype.getRow = function(num) {
@@ -17,21 +17,83 @@ Grid.prototype.getColumn = function(num) {
     }
     return column;
 }
-Grid.prototype.insertElement = function(element,x,y) {
-    let row = (x-1) * this.columns;
-    posXY = row + (y-1);
-    this.array[posXY] = element;
-    document.getElementById(`cell-${y}-${x}-content`).textContent = element;
+Grid.prototype.insertElement = function(element,type,x,y) {
+    let row = (y-1) * this.columns;
+    posXY = row + (x-1);
+    if (element === 'node') {
+        //this.array[posXY] = new MapNode([x,y],this,type)
+        this.array[posXY] = element;
+    } else if (element === 'tile') {
+        this.array[posXY] = new MapTile([x,y],this,type)
+    }
+    document.getElementById(`cell-${x}-${y}-content`).textContent = type;
 }
 Grid.prototype.getElement = function(x,y) {
-    let row = x * this.columns;
-    posXY = row + y;
+    let row = (y-1) * this.columns;
+    posXY = row + (x-1);
     return this.array[posXY];
 }
 Grid.prototype.print = function() {
     createDOMGrid(this);
     drawGrid(this);
     populateGrid(this);
+}
+Grid.prototype.getNodeList = function() {
+    let MapNodeList = [];
+    for (let i=0; i<this.array.length; i++) {
+        if (this.array[i].constructor === MapNode) {
+            MapNodeList.push(this.array[i]);
+        }
+    }
+    return MapNodeList;
+}
+
+function MapNode(coordinate,GridObject,title) {
+    this.title = title;
+    /*this.pageObjects = [];
+    this.pageNum = 0;
+    this.highlighted = false;*/
+    this.linkages = [];
+    this.position = [];
+    if (typeof coordinate === 'object') {
+        this.position = coordinate;
+    }
+}
+MapNode.prototype.addLinkage = function(MapNodeObject) {
+    this.linkages.push(MapNodeObject);
+}
+
+function MapTile(number,GridObject,type) {
+    this.type = type;
+    this.position = [];
+    this.parentGrid = GridObject;
+    let y = Math.floor(number/GridObject.columns);
+    if (typeof number === 'number') {
+        this.position[1] = y + 1;
+        if (y > 0) {
+            this.position[0] = number - (GridObject.columns * y)
+        } else if (y === 0) {
+            this.position[0] = number;
+        }
+    } else if (typeof number === 'object') {
+        this.position = [number[0],number[1]];
+    }
+}
+MapTile.prototype.getAdjacent = function(direction) {
+    switch (direction) {
+        case 'left':
+            return this.parentGrid.getElement((this.position[0]-1),this.position[1]);
+        case 'right':
+            return this.parentGrid.getElement((this.position[0]+1),this.position[1]);
+        case 'above':
+            return this.parentGrid.getElement(this.position[0],this.position[1]-1);
+        case 'below':
+            return this.parentGrid.getElement(this.position[0],this.position[1]-1);
+    }
+}
+MapTile.prototype.setType = function(string) {
+    this.type = string;
+    document.getElementById(`cell-${this.position[0]}-${this.position[1]}-content`).textContent = string;
 }
 
 function generateGridTemplate(GridObject,type) {
@@ -49,8 +111,6 @@ function generateGridTemplate(GridObject,type) {
             return s;
     }
 }
-
-let testMap = new Grid(10,5);
 function createDOMGrid(GridObject) {
     document.querySelector('body').appendChild(document.createElement('div')).id=`GeneratedGrid`;
     const a = document.getElementById('GeneratedGrid')
@@ -60,7 +120,6 @@ function createDOMGrid(GridObject) {
     a.style.gridTemplateRows = generateGridTemplate(GridObject,'rows');
 
 }
-
 function drawGrid(GridObject) {
     let g = document.getElementById('GeneratedGrid')
     g.innerHTML = '';
@@ -90,7 +149,7 @@ function populateGrid(GridObject) {
             p.style.margin = 0;
             p.style.padding = 0;
             p.style.color = `rgb(255,255,255)`
-            p.innerText = GridObject.getColumn(x-1)[y-1];
+            p.innerText = GridObject.getColumn(x-1)[y-1].type;
             p.style.display = 'absolute'
             p.style.left = `${(1/2)*GridObject.cellSize}px`;
             p.style.top = `${(1/2)*GridObject.cellSize}px`;
@@ -100,26 +159,92 @@ function populateGrid(GridObject) {
 
 
 
-function MapNode(title) {
-    this.title = title;
-    /*this.pageObjects = [];
-    this.pageNum = 0;
-    this.highlighted = false;*/
-    this.linkage = [];
-    this.position = [0,0];
+function generateConnections(MapNodeObject,totalNodes) {
+    let w = (2 * totalNodes) + 2;
+    let h = Math.floor((3/4) * w);
+    if (isEven(h) === true) {
+        h += 1;
+    };
+    let mapGrid = new Grid(w,h,'100px');
+    //generate a new grid with an odd height and large enough to contain all given nodes
+    mapGrid.insertElement(MapNodeObject,'node',2,Math.floor(h/2))
+    //place the first node in the middle at the left edge
+    placeNewNode(mapGrid,MapNodeObject);
 }
-MapNode.prototype
+function getRandomInt(max) {
+    return Math.ceil(Math.random() * max);
+}
+function isEven(number) {
+    let check = number/2;
+    const regex = new RegExp('\\.');
+    return !regex.test(''+check);
+};
 
-function MapTile(number,GridObject) {
-    this.type = type;
-    this.position = [];
-    if (number > GridObject.columns) {
-        this.position[]
-    } else if (number < GridObject.columns) {
-        this.position[0] = 1
+//test nodes
+
+function placeNewNode(GridObject,MapNodeObject) {
+
+}
+
+function comparePosition(A,direction,B) {
+    switch (direction) {
+        case 'left':
+            if (A.position[0] < B.position[0]) {
+                return true;
+            } else {
+                return false;
+            }
+        case 'right':
+            if (A.position[0] > B.position[0]) {
+                return true;
+            } else {
+                return false;
+            }
+        case 'above':
+            if (A.position[1] < B.position[1]) {
+                return true;
+            } else {
+                return false;
+            }
+        case 'below':
+            if (A.position[1] > B.position[1]) {
+                return true;
+            } else {
+                return false;
+            }
     }
 }
-
-function compareNodePosition() {
-
+function checkGlobalConnection(x1,y1,x2,y2,GridObject) {
+    
+}
+function checkAdjacentConnection(x,y,GridObject,direction) {
+    let element = GridObject.getElement(x,y);
+    let type = element.type;
+    switch(direction) {
+        //direction is numbered clockwise
+        case 1: //up
+        if (type.includes('^') === true && element.getAdjacent('above').type.includes('v') === true) {
+            return true;
+        } else {
+            return false;
+        }
+        case 2: //right
+        if (type.includes('>') === true && element.getAdjacent('right').type.includes('<') === true) {
+            return true;
+        } else {
+            return false;
+        }
+        case 3: //down
+        if (type.includes('v') === true && element.getAdjacent('below').type.includes('^') === true) {
+            return true;
+        } else {
+            return false;
+        }
+        case 4: //left
+        if (type.includes('<') === true && element.getAdjacent('left').type.includes('>') === true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
